5ef4169da5adc1deb85eafb199447700e69ebfc8
hekaixi <790157506@qq.com>
2019-04-19 14:48:37 +0800

    some update

--- code/src/com/hekx/designMode02/singleton/Test.java
+++ code/src/com/hekx/designMode02/singleton/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("create function ...");
 		System.out.println("create commit ...");
+		System.out.println("aaaaaaaaaaaaa ...");
 	}
 
 }
