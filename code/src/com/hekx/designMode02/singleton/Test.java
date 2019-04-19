3609dcebbe9dd029ec03141198329541506eeb8d
hekaixi <790157506@qq.com>
2019-04-19 14:51:39 +0800

    add a function

--- code/src/com/hekx/designMode02/singleton/Test.java
+++ code/src/com/hekx/designMode02/singleton/Test.java
@@ -5,6 +5,11 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("create function ...");
 		System.out.println("create commit ...");
+		function();
 	}
-
+	
+	
+	public static void function() {
+		System.out.println("xxxxxxxxxxxxxxx");
+	}
 }
