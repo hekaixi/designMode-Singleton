9cfda7d33fdca21616af97651329efdf929e61b4
hekaixi <790157506@qq.com>
2019-04-25 13:57:25 +0800

    some update

--- code/src/com/hekx/designMode02/singleton/Test.java
+++ code/src/com/hekx/designMode02/singleton/Test.java
@@ -3,9 +3,11 @@ package com.hekx.designMode02.singleton;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("create function ...");
-		System.out.println("create commit ...");
-		function();
+//		System.out.println("create function ...");
+//		System.out.println("create commit ...");
+//		function();
+//		
+		System.out.println(16&16);
 	}
 	
 	
